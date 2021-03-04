8aeb1474c7da3bdb88fbc1a6257fb74e46a3dec5
nachiket273 <nachiket.tanksale@gmail.com>
2021-03-04 17:48:13 +0530

    created setup file.

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
+from codecs import open
+import os
 from setuptools import setup, find_packages
 
 
+path = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(path, 'README.md'), encoding='utf-8') as f:
+    desc = f.read()
+
+exec(open(os.path.join(path, 'vistrans', 'version.py')).read())
+
 setup(
     name='vistrans',
-    version='0.0.1',
+    version=__version__,
     description='Unofficial implementations of transfomers models for vision.',
+    long_description=desc,
+    long_description_content_type='text/markdown',
     url='https://github.com/nachiket273/VisTrans',
     author='Nachiket Tanksale',
     author_email='nachiket.tanksale@gmail.com',
@@ -17,7 +27,7 @@ setup(
         'Topic :: Engineering',
         'Topic :: Engineering :: Artificial Intelligence'
     ],
-    keywords='pytorch transformer vision',
+    keywords='pytorch transformer vision python deep-learning',
     packages=find_packages(exclude=[]),
     include_package_data=True,
     install_requires=['torch >= 1.4', 'torchvision'],
