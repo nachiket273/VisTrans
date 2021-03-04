89ec7d86d2110d00fb7de7ef71099a4b0c043311
nachiket273 <nachiket.tanksale@gmail.com>
2021-03-04 20:28:34 +0530

    Fixed container_abcs import issue for torch version > 1.8

--- vistrans/util/utils.py
+++ vistrans/util/utils.py
@@ -3,7 +3,14 @@ import math
 import os
 import torch
 from torch.hub import load_state_dict_from_url
-from torch._six import container_abcs
+
+TORCH_MAJOR = int(torch.__version__.split('.')[0])
+TORCH_MINOR = int(torch.__version__.split('.')[1])
+
+if TORCH_MAJOR == 1 and TORCH_MINOR < 8:
+    from torch._six import container_abcs
+else:
+    import collections.abc as container_abcs
 
 
 # From PyTorch internals (with little change)
